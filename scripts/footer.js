7aa2514814ec41788fe76451d056aa728d2bb8c0
hotkeyuk <60893284+AMG35@users.noreply.github.com>
2024-04-14 16:35:11 +0100

    Correction

--- scripts/footer.js
+++ scripts/footer.js
@@ -1,8 +1,10 @@
 // Text for page footer
-const now = new Date();
-const docdate = new Date(document.lastModified);
+{
+    const now = new Date();
+    const docdate = new Date(document.lastModified);
 
-document.write(
-    "&copy "+ now.getFullYear() +" Hotkey"
-    + " (Last change "+ docdate.toLocaleString() + ")"
-);
+    document.write(
+        "&copy "+ now.getFullYear() +" Hotkey"
+        + " (Last change "+ docdate.toLocaleString() + ")"
+    );
+}
