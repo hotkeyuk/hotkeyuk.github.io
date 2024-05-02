1dcc47ec145f8849b5c3e1325e2b2560f40f5bc9
hotkeyuk <60893284+hotkeyuk@users.noreply.github.com>
2024-05-02 16:00:36 +0100

    header.js Simplified

--- scripts/header.js
+++ scripts/header.js
@@ -1,13 +1,9 @@
 {
-    const output = `
+    document.getElementById("id-header").innerHTML = `
         <div class='left'></div>
         <div class='middle'>
         Hotkey Fan Zone
         </div>
         <div class='right'></div>
-    `       
-    
-    const target = document.getElementById("id-header")
-
-    target.innerHTML = output ;
+    ` ;   
 }    
