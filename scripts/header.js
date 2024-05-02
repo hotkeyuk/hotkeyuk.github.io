fd6bceb73e76671e93954a39679a015be968a5ef
hotkeyuk <60893284+hotkeyuk@users.noreply.github.com>
2024-05-02 15:25:02 +0100

    Added header.js used as <header> all pages

---
+++ scripts/header.js
@@ -0,0 +1,11 @@
+{
+    let output = "<div class='left'></div>" ;
+    output +=   "<div class='middle'>" ;
+    output +=   "Hotkey Fan Zone" ;
+    output +=   "</div>" ;
+    output +=   "<div class='right'></div>" ;         
+    
+    const target = document.getElementById("id-header")
+
+    target.innerHTML = output ;
+}    
